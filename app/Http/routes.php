d559448c2cc95b9d2955669db0b393bf46f60cd4
Thyago A Oliveira <thyagoaoliveira@gmail.com>
2015-10-15 22:56:40 -0300

    Paginação 2

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,4 +6,9 @@ Route::get('sobre', 'BlogController@sobre');
 
 Route::get('contato', 'BlogController@contato');
 
-Route::get('admin', 'BlogAdminController@index');
+Route::get('admin', ['as'=>'admin.index', 'uses'=>'BlogAdminController@index']);
+
+//Route::get('admin/create', 'BlogAdminController@create');
+Route::get('admin/create', ['as'=>'admin.create', 'uses'=>'BlogAdminController@create']);
+
+Route::post('admin/store', ['as'=>'admin.store', 'uses'=>'BlogAdminController@store']);
